9e5a850322618e90f6051fa86f108592b5a6a10c
Cristian Luan Vieira <cristian.luanvieira@gmail.com>
2024-08-20 21:01:18 -0300

    first commit

---
+++ src/test/java/com/lavadush/demo/LavadushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavadush.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavadushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
